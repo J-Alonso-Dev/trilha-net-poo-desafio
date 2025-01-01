1c721bf4fb96d8d336a7f730a4e8def3f694f0fb
J-Alonso-Dev <j.alonso.cardoso@outlook.com>
2025-01-01 15:55:53 -0300

    Entrega do Projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criando um Nokia
+        Console.WriteLine("Testando Nokia:");
+        Smartphone nokia = new Nokia("123456789", "Nokia G21", "111111111", 128);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        // Criando um iPhone
+        Console.WriteLine("\nTestando iPhone:");
+        Smartphone iphone = new Iphone("987654321", "iPhone 13", "222222222", 256);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
